41a49315e33a9a08b6dcf462aee7a5f47ffa6299
orekihoutaro <orekihoutaro1218@gmail.com>
2023-04-29 14:17:09 +0800

    Added secret Key

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'replace-with-your-secret-key'
+app.config['SECRET_KEY'] = 'asdfghjklqwertyuiop'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
