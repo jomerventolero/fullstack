8b478c3c3d4c1cb5b69392e5f88e416356ba6208
orekihoutaro <orekihoutaro1218@gmail.com>
2023-04-30 22:55:28 +0800

    Added some updates

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import herobg from '../assets/herobg.jpg'
 import keys from '../assets/keys.png'
 import { motion } from 'framer-motion'
+import herobg2 from '../assets/herobg2.png'
 
 const Hero = () => {
   return (
@@ -18,7 +19,7 @@ const Hero = () => {
         initial={{ x: "-50vw"}}
         animate={{ x:0 }}
         transition={{ type: "spring", stiffness: 40, delay: 0.4}}
-        className="absolute top-[280px] left-[100px] text-[64px] font-poppins font-semibold text-white">C E L I N A</motion.h1>
+        className="absolute top-[280px] left-[100px] text-[64px] font-poppins font-semibold text-white drop-shadow-2xl">C E L I N A</motion.h1>
       <motion.h1 
           initial={{ y: "150vw"}}
           animate={{ y:0 }}
@@ -45,6 +46,14 @@ const Hero = () => {
         >
         <img src={keys} alt="keys" className="w-1/4 absolute top-[100px] left-[20px]"/>
       </motion.div>
+      <motion.div
+        initial={{ x: "50vw" }}
+        animate={{ x:0 }}
+        transition={{ type: "spring", stiffness: 80, delay: 0.8}}
+        className="-z-40"
+        >
+        <img src={herobg2} alt="hero" className="w-1/2 absolute top-[60px] right-2 -z-10"/>
+      </motion.div>  
     </div>
   )
 }
