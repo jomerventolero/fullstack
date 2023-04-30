4f868e83283d5d1978af9687a3045755c5a7f729
orekihoutaro <orekihoutaro1218@gmail.com>
2023-04-30 22:54:20 +0800

    Flask Backend

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'asdfghjklqwertyuiop'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
@@ -16,7 +17,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
