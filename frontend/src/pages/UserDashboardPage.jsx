89237a0f1806f1687d616883064fcb83ade3cae4
orekihoutaro <orekihoutaro1218@gmail.com>
2023-05-15 16:46:52 +0800

    Dashboard for users

---
+++ frontend/src/pages/UserDashboardPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardNav from '../components/DashboardNav'
+
+const UserDashboardPage = () => {
+  return (
+    <div>
+        <DashboardNav />
+        
+    </div>
+  )
+}
+
+export default UserDashboardPage
